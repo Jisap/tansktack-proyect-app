55300757970d8daec9ab2a71762c22c8e0af72ba
Jorge Matos <jisapjisap@gmail.com>
2025-12-27 16:11:54 +0100

    Limit displayed products to the first six in the main app and implement product fetching in the products route for enhanced performance and user experience.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -85,7 +85,7 @@ export function App() {
             </div>
           </div>
           <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3 mb-6">
-            {products.map((product, index) => (
+            {products.slice(0,6).map((product, index) => (
               <ProductCard 
                 product={product} 
                 key={`product-${index}`} 

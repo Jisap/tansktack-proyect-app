ab9e43b57a14573a50d15ca9cb29aaa281f6e5c3
Jorge Matos <jisapjisap@gmail.com>
2025-12-27 17:07:26 +0100

    Add link component and default not found handler in router; implement logging middleware and POST handler in products route

--- src/routes/products/index.tsx
+++ src/routes/products/index.tsx
@@ -2,17 +2,40 @@ import ProductCard from '@/components/Product-card'
 import { Card, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import { sampleProducts } from '@/db/seed'
 import { createFileRoute} from '@tanstack/react-router'
-import { createServerFn } from '@tanstack/react-start'
+import { createMiddleware, createServerFn, json } from '@tanstack/react-start'
 
 
 const fetchProductsFn = createServerFn({ method: 'GET' }).handler(async () => {
   return { products: sampleProducts }
 })
 
+const loggerMiddleware = createMiddleware().server(
+  async ({ next, request }) => {
+    // 1. ACCIÓN: Aquí hacemos algo antes de continuar.
+    console.log(
+      '---loggerMiddleware---',
+      request.url,                    // Vemos a qué URL quieren ir
+      'from',
+      request.headers.get('origin'),  // Vemos de dónde vienen
+    )
+    // 2. PASE: Dejamos que la petición continúe su camino
+    return next()
+  },
+)
+
 export const Route = createFileRoute('/products/')({
   component: RouteComponent,
   loader: async () => {
     return fetchProductsFn()
+  },
+  server: {
+    middleware: [loggerMiddleware],
+    handlers: {
+      POST: async ({ request }) => {
+        const body = await request.json()
+        return json({ message: 'Hello, world from POST request!', body })
+      },
+    },
   }
 })
 
